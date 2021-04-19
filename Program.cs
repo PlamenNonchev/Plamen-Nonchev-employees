fa8889c4e1bac71537b9406393616923e30ccb6b
PlamenNonchev <plamen.nonchev@gmail.com>
2021-04-19 12:12:02 +0300

    Added ProjectEmployee class + ProjectEmployee Collection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BestColleagueDuo
 {
@@ -8,10 +9,28 @@ namespace BestColleagueDuo
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\employeeProjects.txt");
 
+            List<ProjectEmployee> projectEmployees = new List<ProjectEmployee>();
+
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                string[] splitLine = line.Split(",");
+                int projectId = int.Parse(splitLine[0]);
+                int employeeId = int.Parse(splitLine[1]);
+                DateTime dateFrom = DateTime.Parse(splitLine[2]);
+                DateTime dateTo;
+                if (splitLine[3] == "NULL")
+                {
+                    dateTo = DateTime.Now.Date;
+                }
+                else
+                {
+                    dateTo = DateTime.Parse(splitLine[3]);
+                }
+                ProjectEmployee pe = new ProjectEmployee(projectId, employeeId, dateFrom, dateTo);
+                projectEmployees.Add(pe);
             }
+
+
         }
     }
 }
---
+++ ProjectEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestColleagueDuo
+{
+    public class ProjectEmployee
+    {
+        public ProjectEmployee(int projectId, int employeeId, DateTime dateFrom, DateTime dateTo)
+        {
+            ProjectId = projectId;
+            EmployeeId = employeeId;
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+        }
+
+        public int ProjectId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+    }
+}
