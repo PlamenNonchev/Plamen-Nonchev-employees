02b41e97aedd153a739361848cdac22336697859
PlamenNonchev <plamen.nonchev@gmail.com>
2021-04-19 13:59:24 +0300

    Added EmployeeDuo class + calculation logic for overlapping work days for a specific project + finding the duo that has worked together the most
    The project is complete from a functional standpoint, however I still need to do code refactoring and optimization.

---
+++ EmployeeDuo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestColleagueDuo
+{
+    public class EmployeeDuo
+    {
+        public EmployeeDuo(int employee1Id, int employee2Id, int workedTogetherPeriod )
+        {
+            Employee1Id = employee1Id;
+            Employee2Id = employee2Id;
+            WorkedTogetherPeriod = workedTogetherPeriod;
+        }
+        public int Employee1Id { get; set; }
+
+        public int Employee2Id { get; set; }
+
+        public int WorkedTogetherPeriod { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BestColleagueDuo
 {
@@ -30,7 +31,73 @@ namespace BestColleagueDuo
                 projectEmployees.Add(pe);
             }
 
+            List<EmployeeDuo> employeeDuos = new List<EmployeeDuo>();
+            var projectIds = projectEmployees.Select(p => p.ProjectId).Distinct().ToList();
 
+            foreach (var projectId in projectIds)
+            {
+                var employeeIds = projectEmployees.Where(p => p.ProjectId == projectId).Select(e => e.EmployeeId).OrderBy(x => x).ToArray();
+                
+
+                for (int i = 0; i < employeeIds.Length; i++)
+                {
+                    int emp1 = employeeIds[i];
+                    var projEmp1 = projectEmployees.FirstOrDefault(pe => pe.ProjectId == projectId && pe.EmployeeId == emp1);
+                    DateTime startDate1 = projEmp1.DateFrom;
+                    DateTime endDate1 = projEmp1.DateTo;
+                    for (int j = i + 1; j < employeeIds.Length; j++)
+                    {
+                        
+                        int emp2 = employeeIds[j];
+                        var projEmp2 = projectEmployees.FirstOrDefault(pe => pe.ProjectId == projectId && pe.EmployeeId == emp2);
+                        DateTime startDate2 = projEmp2.DateFrom;
+                        DateTime endDate2 = projEmp2.DateTo;
+
+                        bool overlap = startDate1 < endDate2 && startDate2 < endDate1;
+                        if (overlap)
+                        {
+                            DateTime overlapStart;
+                            DateTime overlapEnd;
+                            if (startDate1 < startDate2)
+                            {
+                                overlapStart = startDate2;
+                            }
+                            else
+                            {
+                                overlapStart = startDate1;
+                            }
+
+                            if (endDate1 < endDate2)
+                            {
+                                overlapEnd = endDate1;
+                            }
+                            else
+                            {
+                                overlapEnd = endDate2;
+                            }
+
+                            int overlapDays = (overlapEnd - overlapStart).Days;
+
+                            var empDuo = employeeDuos.FirstOrDefault(ed => ed.Employee1Id == emp1 && ed.Employee2Id == emp2);
+                            if(empDuo == null)
+                            {
+                                EmployeeDuo ed = new EmployeeDuo(emp1, emp2, overlapDays);
+                                employeeDuos.Add(ed);
+                            }
+                            else
+                            {
+                                empDuo.WorkedTogetherPeriod += overlapDays;
+                            }
+                        }
+                        
+
+
+                    }
+                }
+            }
+
+            var bestDuo = employeeDuos.OrderByDescending(ed => ed.WorkedTogetherPeriod).First();
+            Console.WriteLine($"Employee {bestDuo.Employee1Id} and employee {bestDuo.Employee2Id} make the best duo!\nThey have worked together for a total of {bestDuo.WorkedTogetherPeriod} days");
         }
     }
 }
